9225e57de3f897448b548968e2f10a52b15db4f1
jsanabrh <juanhoyos0422@gmail.com>
2024-05-30 11:58:37 -0500

    Database development begins

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [],
+      extra: {
+        ssl: true,
+      },
+    }),
+    TypeOrmModule.forFeature([]), // Register your entities
+  ],
   controllers: [],
   providers: [],
 })
